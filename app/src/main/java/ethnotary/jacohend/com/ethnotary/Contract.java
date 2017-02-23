b740ccfafa0df995bfbae1bc5dc64db7ff689238
Jacob Henderson <jacob@joinsnapcard.com>
2017-02-23 14:06:24 -0800

    wip, builds and runs

--- app/src/main/java/ethnotary/jacohend/com/ethnotary/Contract.java
+++ app/src/main/java/ethnotary/jacohend/com/ethnotary/Contract.java
@@ -4,7 +4,6 @@ package ethnotary.jacohend.com.ethnotary;
  * Created by jacob on 2/17/17.
  */
 public interface Contract {
-
     void add(String title, String address, String document);
     String getDoc(String title);
     long getTimestamp(String title);
